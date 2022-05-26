00f21d701b824f58f3d838d3b5dbca965c33c0eb
Michel Erokhov <mixaileroxov@gmail.com>
2022-05-26 12:32:07 +0300

    handle DeleteMerkleRoot events

--- src/helpers/getLedger.ts
+++ src/helpers/getLedger.ts
@@ -1,18 +1,22 @@
-import { QUERY_BLOCK_LIMIT } from '@big-whale-labs/constants'
 import { SealCredLedger } from '@big-whale-labs/seal-cred-ledger-contract'
+import getAllEvents from '@/helpers/getAllEvents'
 import getLedgerRecord from '@/helpers/getLedgerRecord'
 import ledger from '@/helpers/ledger'
 
 export default async function getLedger(sealCredLedger: SealCredLedger) {
-  const eventsFilter = sealCredLedger.filters.SetMerkleRoot()
-  const events = await sealCredLedger.queryFilter(
-    eventsFilter,
-    QUERY_BLOCK_LIMIT
-  )
+  const { events, deleteTopic } = await getAllEvents(sealCredLedger)
   const addressToMerkle: { [address: string]: string } = {}
 
   for (const event of events) {
-    const { tokenAddress, merkleRoot } = event.args
+    const {
+      args: { tokenAddress, merkleRoot },
+      topic,
+    } = event
+    if (deleteTopic === topic) {
+      delete addressToMerkle[tokenAddress]
+      continue
+    }
+
     addressToMerkle[tokenAddress] = merkleRoot
   }
 
